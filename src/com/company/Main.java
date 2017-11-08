e55387fff9aeb4d15da9a5793cc9b032cb30ed7d
Kirill <31863181+llirrikk@users.noreply.github.com>
2017-11-08 22:10:14 +0300

    Fixed and corrected the sign '-'. Revised program structure. Now you can do several actions (there were only two).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static int num (String num) {
+    public static int num(String num) {
         int p = Integer.parseInt(num);
         return p;
     }
@@ -13,27 +13,33 @@ public class Main {
 
     public static void main(String[] args) {
         int z = 0;
-        String num1 = "", num2 = "";
+        char[] array = new char[100];
+        int[] array2 = new int[100];
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        for (int j = 0; j < str.length(); j++) {
-            if (str.charAt(j) == '+' || str.charAt(j) == '-') {
-                num1 = str.substring (0,j);
-                char sings = str.charAt(j);
-                z = j;
-                break;
+        String myString = in.nextLine();
+        for (int j = 0; j < myString.length(); j++) {
+            if (myString.charAt(j) == '+' || myString.charAt(j) == '-' || myString.charAt(j) == '=') { // В конце примера нужно поставить знак '='
+                array2[z] = num(myString.substring(0, j));
+                array[z] = myString.charAt(j);
+                z++;
+                myString = remove(myString, j);
+                j = 0;
             }
         }
-        str = remove(str, z);
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '+' || str.charAt(i) == '-' || str.charAt(i) == '=') { // В конце примера нужно поставить знак '='
-                num2 = str.substring(0, i);
-                break;
+        int answer = 0;
+        z = 0;
+        answer = array2[0];
+        for (int i = 1; i < 99; i++) {
+            if (array[z] == '+') {
+                answer = answer + array2[i];
+                z++;
+            } else {
+                if (array[z] == '-') {
+                    answer = answer - array2[i];
+                    z++;
+                }
             }
         }
-        int u = num (num1);
-        int v = num (num2);
-        System.out.print(u + v);
-
+        System.out.print(answer);
     }
 }
